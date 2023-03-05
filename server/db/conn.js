5241ce9478313f10530d4d956458ec2a99191f0d
Alexander <wintersalex@icloud.com>
2023-03-04 16:03:42 -0800

    Change database connection to mongoose

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,29 +1,16 @@
-const { MongoClient } = require("mongodb");
+const mongoose = require('mongoose');
 const url = process.env.ATLAS_URI
 
-let _db;
-
-module.exports = {
-  connectToServer: function () {
+module.exports = (mongoose) => {
     return new Promise((resolve, reject) => {
-      MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
-        .then(client => {
-          _db = client.db();
-          console.log("Connected to MongoDB");
+      mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+        .then(() => {
+          console.log("Connected to MongoDB using Mongoose");
           resolve();
         })
-        .catch(err => {
-          console.error("Error connecting to MongoDB", err);
+        .catch((err) => {
+          console.error("Error connecting to MongoDB using Mongoose", err);
           reject(err);
         });
     });
-  },
-
-  getDb: function () {
-    if (!_db) {
-      console.warn("Attempted to get database before connecting.");
-      return null;
-    }
-    return _db;
-  }
-};
+  };
--- server/server.js
+++ server/server.js
@@ -7,16 +7,17 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 app.use(require("./routes/dashboard"));
-// get driver connection
-const dbo = require("./db/conn");
+// Get Mongoose connection
+const mongoose = require('Mongoose');
+const db = require("./db/conn");
 
-dbo.connectToServer()
+db(connectToServer)
   .then(() => {
     app.listen(port, () => {
       console.log(`Server is running on port: ${port}`);
     });
   })
-  .catch(err => {
+  .catch((err) => {
     console.error("Failed to connect to MongoDB", err);
     process.exit(1);
   });
