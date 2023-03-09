e43f9535e1ce33d86f79199eaaeb03d1b03a18c2
Alexander <wintersalex@icloud.com>
2023-03-09 15:57:51 -0800

    Add the first user function to create_user

--- server/db/db_functions/user_functions.js
+++ server/db/db_functions/user_functions.js
@@ -1,14 +1,20 @@
 const User = require('../models/user');
+require("dotenv").config({ path: "../../config.env" });
+const connectDB = require('../conn')
+
+// Connect to MongoDB
+connectDB();
 
 async function create_user(username) {
     try {
         // Create a new user with provided username
-        let new_user = new User({ username });
+        const new_user = new User({ username });
 
         // Save the new user to the database
         await new_user.save();
 
         // Return the newly created user
+        console.log(new_user);
         return new_user;
 
     } catch (error) {
@@ -17,5 +23,7 @@ async function create_user(username) {
         throw new Error('Error creating user');
     }
 }
+
+create_user('olivier');
 
 module.exports = { create_user };
