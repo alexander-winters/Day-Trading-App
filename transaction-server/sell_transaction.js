11edfa6ce5a94833c3d461b1891d0b0fd36842e8
Elmer Martinez <yomer237@uvic.ca>
2023-03-08 18:14:41 -0800

    Write pseudocode for set sell commands

--- transaction-server/sell_transaction.js
+++ transaction-server/sell_transaction.js
@@ -2,6 +2,7 @@ const quote_server = require("../quote-server/quote_server");
 const utils = require("./utils");
 
 let transaction = utils.initialize_transaction();
+let set_sell_pending;
 
 async function sell(user, stock_symbol, amount) {
     const quote = await quote_server.get_quote(stock_symbol, user);
@@ -13,7 +14,7 @@ async function sell(user, stock_symbol, amount) {
 
     // Verify user have enough stocks to sell
     if (user.stocks[stock_symbol].value < amount) {
-        return({error: "Insufficient stocks owned"})
+        return({error: "Insufficient stocks to sell"})
     }
 
     const sell_qty = amount/quote.Quoteprice;
@@ -24,7 +25,7 @@ async function sell(user, stock_symbol, amount) {
     transaction.amount = amount;
     transaction.sell_time = Date.now();
 
-    return ({success: "Please confirm or cancel the transaction" });
+    return ({success: "Please confirm or cancel the sell transaction" });
 }
 
 async function commit_sell(user) {
@@ -36,7 +37,7 @@ async function commit_sell(user) {
         
         transaction = utils.initialize_transaction(); // resets transaction
 
-        return({success: "Transaction was successful"})
+        return({success: "Sell transaction was successful"})
     }    
 
     return({error: "No sell transaction initiated within 60 seconds"})
@@ -45,22 +46,44 @@ async function commit_sell(user) {
 async function cancel_sell(user) {
     if (transaction.type == 'SELL' && (Date.now() - transaction.sell_time <= (60 * 1000))) {
         transaction = utils.initialize_transaction(); // resets transaction
-        return({success: "Transaction canceled"})
+        return({success: "Sell transaction canceled"})
     }
     
     return ({error: "No sell transaction initiated within 60 seconds"})
 }
 
 async function set_sell_amount(user, stock_symbol, amount) {
-    // Complete
+    let user_stock_value = 1000;
+    
+    if (user_stock_value >= amount) {
+        set_sell_pending = true;
+        return ({success: "Set sell trigger initialized"});
+    }
+
+    return({error: "Insufficient stocks to sell"})
 }
 
 async function set_sell_trigger(user, stock_symbol, amount) {
-    // Complete
+    if (set_sell_pending == true) {
+        // TODO
+        // Create a reserve account for the specified amount of the given stock
+        // Reduce specified user owned stocks
+        // Add trigger to user's sell triggers
+        return ({success: `Sell transaction was set at ${amount}`});
+    }
+
+    return({error: "Please initialize a set sell trigger first"});
 }
 
 async function cancel_set_sell(user, stock_symbol, amount) {
-    // Complete
+    if (set_sell_pending == true) {
+        // TODO
+        // Remove associated sell trigger
+        // Restore user account information values
+        return ({success: "Set sell trigger was successfully canceled"});
+    }
+
+    return({error: "No set sell trigger initialized"});
 }
 
 module.exports = { sell,
