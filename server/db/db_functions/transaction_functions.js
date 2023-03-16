bf80778a4c99d447c8a39429bdcffe9f6bbb9676
Elmer Martinez <yomer237@uvic.ca>
2023-03-16 12:19:24 -0700

    Generate timestamp when creating transactions

--- server/db/db_functions/transaction_functions.js
+++ server/db/db_functions/transaction_functions.js
@@ -13,7 +13,9 @@ async function create_transaction(userid, user, logType, request = {}, response 
             log_type: logType, 
             user_request: request, 
             server_response: response,
-            server: server
+            server: server,
+            transaction_timestamp: Math.floor(new Date().getTime()),
+            transaction_expires: Math.floor(new Date().getTime()) + (60 * 1000)
         });
 
         await transaction.save();
