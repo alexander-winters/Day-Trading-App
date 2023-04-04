f56a8afac9cf2bd03deee2911ddaea01915de4ac
Alexander <wintersalex@icloud.com>
2023-04-04 13:04:58 -0700

    Modify nginx.conf Modify payload to use less threads Modify docker-compose Modify ecosystem.config.js

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -1,17 +1,15 @@
 module.exports = {
     apps: [
       {
-        name: 'my-server',
-        script: 'server/server.js',
-        instances: 'max',
+        name: 'server',
+        script: './server/server.js',
+        instances: max,
+        exec_mode: 'cluster',
         autorestart: true,
         watch: false,
         max_memory_restart: '1G',
         env: {
-          NODE_ENV: 'development',
-        },
-        env_production: {
-          NODE_ENV: 'production',
+          PORT: 5000,
         },
       },
     ],
