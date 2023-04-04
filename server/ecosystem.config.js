5423678148897932a3bb0eda78f6c4a9345adfa6
Alexander <wintersalex@icloud.com>
2023-04-04 09:32:22 -0700

    Add ecosystem.config.js and modify dockerfile to use PM2

---
+++ server/ecosystem.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    apps: [
+      {
+        name: 'my-server',
+        script: 'server.js',
+        instances: 'max',
+        autorestart: true,
+        watch: false,
+        max_memory_restart: '1G',
+        env: {
+          NODE_ENV: 'development',
+        },
+        env_production: {
+          NODE_ENV: 'production',
+        },
+      },
+    ],
+  };
