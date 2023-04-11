59768f54bced4415cb2cbda21a158136c398b717
Alexander <wintersalex@icloud.com>
2023-04-10 17:52:56 -0700

    Fix error in ecosystem.config.js

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -9,7 +9,7 @@ module.exports = {
         watch: false,
         max_memory_restart: '1G',
         env: {
-            NODE_ENV: "developement",
+            NODE_ENV: "development",
         },
         env_production: {
             NODE_ENV: "production",
