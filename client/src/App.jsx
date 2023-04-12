455b05308bf53f8f8892bd8ffa406aad7ee4f944
Elmer Martinez <yomer237@uvic.ca>
2023-04-11 17:46:51 -0700

    Landing Page Initial Implementation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,38 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-import Navbar from './components/Navbar'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+import React from 'react';
+import Home from './components/home';
+import Login from './components/Login';
+const App = () => {
   return (
-    <div className="App">
-      <Navbar />
-      <p>Testing 123</p>
-      
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-  </p>
+    <div className='Application grid grid-cols-2 divide-x h-[720px]'>
+      <Home />
+      <Login/>
     </div>
-  )
+  );
 }
+
 
 export default App
