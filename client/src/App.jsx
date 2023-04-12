04568be178f7bf96d6e61144f441a662b7ca84ce
Alexander <wintersalex@icloud.com>
2023-04-12 15:26:09 -0700

    Create a working dummy dashboard

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,32 @@
+// App.jsx
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import Home from './components/home';
-import Login from './components/Login';
+import Dashboard from './components/Dashboard';
+
 const App = () => {
   return (
-    <div className='Application grid grid-cols-2 divide-x h-[720px]'>
-      <Home />
-      <Login/>
+    <div className="Application">
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div className="h-screen w-full">
+              <Home />
+            </div>
+          }
+        />
+        <Route
+          path="/dashboard"
+          element={
+            <div className="h-screen w-full">
+              <Dashboard />
+            </div>
+          }
+        />
+      </Routes>
     </div>
   );
-}
+};
 
-
-export default App
+export default App;
--- client/src/components/home.jsx
+++ client/src/components/home.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import logo from '../assets/logo.png';
+import Login from './Login';
 
 const Home = () => {
   return (
-    <div className='logo-container flex-shrink-0 bg-neutral-100 items-center py-24'>
-      <img src={logo} alt="app logo" className='w-[565px] min-w-50% h-[369px] min-h-50%' />
+    <div className="home-container" style={{ display: 'flex' }}>
+      <div className='logo-container flex-shrink-0 bg-neutral-100 items-center py-24'>
+        <img src={logo} alt="app logo" className='w-[565px] min-w-50% h-[369px] min-h-50%' />
+      </div>
+      <Login />
     </div>
   )
 }
